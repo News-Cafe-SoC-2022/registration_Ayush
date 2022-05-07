43bf1d70554a528846b25187da28c79af600b6af
Ayush Agarwal <96474183+ChopstickAyush@users.noreply.github.com>
2022-05-07 15:14:07 +0530

    Add files via upload

--- urls.py
+++ urls.py
@@ -1,6 +1,30 @@
-from django.urls import path
-from . import views
+"""newscaffee URL Configuration
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/2.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from tempfile import template
+from django.contrib import admin
+from django.contrib.auth import views as auth_views
+from django.urls import path , include, reverse_lazy
+from user import views as user_views
+
 urlpatterns = [
-    path('', views.home,name='blog-home'),
-    path('about/', views.about,name='about-home'),
+    path('admin/', admin.site.urls),
+    path('', include('blog.urls')),
+    path('register/', user_views.register , name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='user/login.html'),name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='user/logout.html'),name='logout'),
+    path('changepassword/', auth_views.PasswordChangeView.as_view(template_name='user/changepassword.html'),name='changepassword'),
+    path('password_change/done/', auth_views.PasswordChangeView.as_view(template_name='user/password_change_done.html'),name='password_change_done'),
 ]
